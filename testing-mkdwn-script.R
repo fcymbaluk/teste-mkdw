562406e1fca66a5c11d000fa9166ced43ea43d62
fcymbaluk <fcymbaluk@gmail.com>
2025-01-04 12:34:46 -0300

    Fixed rendering error

--- testing-mkdwn-script.R
+++ testing-mkdwn-script.R
@@ -1,21 +1,3 @@
-#'## R Markdown
-
-#'This is an R Markdown document. Markdown is a simple formatting syntax for authoring HTML, PDF, and MS Word documents. For more details on using R Markdown see <http://rmarkdown.rstudio.com>.
-
-#'When you click the **Knit** button a document will be generated that includes both content as well as the output of any embedded R code chunks within the document. You can embed an R code chunk like this:
-
-#+ r setup, include = FALSE
-summary(cars)
-
-#'## Including Plots
-
-#'You can also embed plots, for example:
-
-#+ r setup, include = FALSE
-plot(pressure)
-
-#'Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
-
 #' Here's some prose in a very special comment. Let's summarize the built-in
 #' dataset `VADeaths`.
 # here is a regular code comment, that will remain as such
